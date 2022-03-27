0656433af881372c7c4b3994b9bd3a9ad5e96394
Farhan Sayed <farhanthegamer162@gmail.com>
2022-03-27 10:59:47 +0400

    Get All Movies & Build Rows

---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+function Banner() {
+  const [movie, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {}
+    fetchData();
+  }, []);
+
+  return <header></header>;
+}
+
+export default Banner;
